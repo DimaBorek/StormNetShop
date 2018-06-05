abe06a1b76a3ca2972344c714629b3e23ce46646
DimaBorek <drackon4ik@gmail.com>
2018-06-05 22:00:34 +0300

    Initial commit

---
+++ backend/src/main/java/Controller.java
@@ -0,0 +1,64 @@
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+public class Controller {
+    private static Route getFindGoodByNameRoute() {
+        Route getRoute = new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String goodName = request.queryParams("goodName");
+                if (StringUtils.isBlank(goodName)) {
+                    return "Please specify correct good name";
+                }
+                Good good = ShopDAO.findByName(goodName);
+                if (good == null) {
+                    return "Good with name " + goodName + " not found";
+                }
+                String json = mapper.writeValueAsString(good);
+                return json;
+            }
+        };
+        return getRoute;
+    }
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    private static Route getAllGoodsRoute() {
+        Route getRoute = new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                String allGoods = mapper.writeValueAsString(ShopDAO.findAllGoods());
+                return allGoods;
+            }
+        };
+        return getRoute;
+
+    }
+
+    public static void main(String[] args) {
+        get("/getAllGoods", getAllGoodsRoute());
+        get("/getAllGoods", getAllGoodsRoute());
+        get("/findGoodByName", getFindGoodByNameRoute());
+
+   /*     Route getRoute = new Route() {
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "This is GET reguest";
+            }
+        };
+        Route postRoute = new Route(){
+            @Override
+            public Object handle(Request request, Response response) throws Exception {
+                return "This is POST reguest";
+            }
+        };
+        get("/SimpleGet",getRoute);
+        post("/SimplePost",postRoute);*/
+}
+}
